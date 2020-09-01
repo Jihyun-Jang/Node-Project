74eb36696a4cb2b916ef72a1bb846870dead112a
Jihyun-Jang <jhjang742@gmail.com>
2020-09-01 17:47:53 +0100

    Made form and style

--- server.js
+++ server.js
@@ -2,7 +2,20 @@ const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  res.end('Hello, I am making my own project!');
+  const url = req.url;
+  if (url === '/') {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.end('<h1>This is your first page</h1>');
+  } else if (url === '/goodbye') {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.end('<h1>Goodbye page</h1>');
+  } else if (url === '/homepage') {
+    res.writeHead(302, { location: '/' });
+    res.end();
+  } else {
+    res.writeHead(404, { 'content-type': 'text/html' });
+    res.end('<h1>Page not found</h1>');
+  }
 });
 
 server.listen(PORT, () => {
