5e3ef01160b41c82dc5adef29cef88371b5e6164
Jihyun-Jang <jhjang742@gmail.com>
2020-09-01 12:48:51 +0100

    Create server

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-const http = reqire("http");
+const http = require('http');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.end('Hello, I am making my own project!');
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
