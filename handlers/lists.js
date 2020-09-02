b65446a47829a63f785b221a91357cb550628070
Jihyun-Jang <jhjang742@gmail.com>
2020-09-02 18:24:47 +0100

    Display the data

--- handlers/lists.js
+++ handlers/lists.js
@@ -1,16 +1,31 @@
 const fs = require('fs');
 const path = require('path');
+const database = require('../model');
 
 function listsHandler(req, res) {
   const filePath = path.join(__dirname, '..', 'public', 'display.html');
+
   fs.readFile(filePath, 'utf-8', (error, html) => {
     if (error) {
       console.log(error);
       res.writeHead(404, { 'content-type': 'text/html' });
       res.end('<h1> Sorry, page is not found</h1>');
     } else {
+      const article = database.map((thoughtsObj) => {
+        return `<article>
+        <p>${thoughtsObj.name}</p>
+        <p>${thoughtsObj.thoughts}</p>
+        <p>${thoughtsObj.date}</p>
+      </article>`;
+      });
+
+      const newFile = html.replace(
+        `<!-- <article> comes here -->`,
+        article.join('\n')
+      );
+
       res.writeHead(200, { 'content-type': 'text/html' });
-      res.end(html);
+      res.end(newFile);
     }
   });
 }
